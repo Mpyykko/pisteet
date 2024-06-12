6b8ed4af46344018fed0c6950cae1daee447bf07
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-06-12 15:11:58 +0300

    update

--- main.js
+++ main.js
@@ -47,3 +47,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
+const paivamaaraElement = document.getElementById('paivamaara');
+
+
+function muotoilePaivamaara() {
+  const nyt = new Date();
+  const vuosi = nyt.getFullYear();
+  const kuukausi = nyt.getMonth() + 1;
+  const paiva = nyt.getDate();
+
+  paivamaaraElement.textContent = ` Tänään on ${paiva.toString().padStart(2, '0')}.${kuukausi.toString().padStart(2, '0')}.${vuosi.toString().padStart(2, '0')}`;
+
+}
+
+
+muotoilePaivamaara();
